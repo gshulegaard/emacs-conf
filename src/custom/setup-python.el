3239c2b2fac23558c008e85494c1745634856bb0
gshulegaard <loki.labrys@gmail.com>
2023-04-28 16:59:18 -0400

    feat: setup pyvenv

--- src/custom/setup-python.el
+++ src/custom/setup-python.el
@@ -39,5 +39,40 @@
 (use-package lsp-pyright
   :straight t)
 
+;; pyvenv
+
+(use-package pyvenv
+  :straight t
+  :ensure t
+  :init
+  (setenv "WORKON_HOME" "./venv/")
+  :config
+  ;; (pyvenv-mode t)
+
+  ;; Set correct Python interpreter
+  (setq pyvenv-post-activate-hooks
+        (list (lambda ()
+                (setq python-shell-interpreter (concat pyvenv-virtual-env "bin/python")))))
+  (setq pyvenv-post-deactivate-hooks
+        (list (lambda ()
+                (setq python-shell-interpreter "python3")))))
+
+;; Setup some python-mode hooks
+;; https://github.com/daviwil/emacs-from-scratch/wiki/LSP-Python-(pyright)-config-in-emacs-from-scratch#python-mode
+
+(use-package python-mode
+  :straight t
+  :hook
+  (python-mode . pyvenv-mode)
+  (python-mode . flycheck-mode)
+  (python-mode . company-mode)
+  ;(python-mode . blacken-mode)
+  ;(python-mode . yas-minor-mode)
+  :custom
+  ;; NOTE: Set these if Python 3 is called "python3" on your system!
+  (python-shell-interpreter "python3")
+  :config
+  )
+
 (provide 'setup-python)
 ;;; setup-python.el ends here
